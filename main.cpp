e33d125a0c0ba3189440288048c81f4526dc9016
beejdrik <brntfader@gmail.com>
2026-04-21 02:39:55 -0400

    added user inputs

--- main.cpp
+++ main.cpp
@@ -1,52 +1,43 @@
 #include <iostream>
 #include "dijkstraAlg.h"
 #include "Graph.h"
+#include "mapBuilder.h"
 using namespace std;
 
 int main() {
 
     Graph g;
 
-    //EXAMPLE ------------------------------------------------------------------------------
-
-    // Row 0
-    g.addEdge(0, 1, 1);   g.addEdge(1, 0, 1);
-    g.addEdge(1, 2, 1);   g.addEdge(2, 1, 1);
-    g.addEdge(2, 3, 50);   g.addEdge(3, 2, 1);   //EDIT TO 50 *******
-
-    // Row 1
-    g.addEdge(4, 5, 1);   g.addEdge(5, 4, 1);
-    g.addEdge(5, 6, 1);   g.addEdge(6, 5, 1);
-    g.addEdge(6, 7, 1);   g.addEdge(7, 6, 1);
-
-    // Row 2
-    g.addEdge(8, 9, 1);   g.addEdge(9, 8, 1);
-    g.addEdge(9, 10, 1);  g.addEdge(10, 9, 1);
-    g.addEdge(10, 11, 1); g.addEdge(11, 10, 1);
-
-    // Row 3
-    g.addEdge(12, 13, 1); g.addEdge(13, 12, 1);
-    g.addEdge(13, 14, 1); g.addEdge(14, 13, 1);
-    g.addEdge(14, 15, 1); g.addEdge(15, 14, 1);
-
-    // Columns
-    g.addEdge(0, 4, 1);   g.addEdge(4, 0, 1);
-    g.addEdge(1, 5, 1);   g.addEdge(5, 1, 1);
-    g.addEdge(2, 6, 1);   g.addEdge(6, 2, 1);
-    g.addEdge(3, 7, 1);   g.addEdge(7, 3, 1);
-    g.addEdge(4, 8, 1);   g.addEdge(8, 4, 1);
-    g.addEdge(5, 9, 1);   g.addEdge(9, 5, 1);
-    g.addEdge(6, 10, 1);  g.addEdge(10, 6, 1);
-    g.addEdge(7, 11, 1);  g.addEdge(11, 7, 1);
-    g.addEdge(8, 12, 1);  g.addEdge(12, 8, 1);
-    g.addEdge(9, 13, 1);  g.addEdge(13, 9, 1);
-    g.addEdge(10, 14, 1); g.addEdge(14, 10, 1);
-    g.addEdge(11, 15, 1); g.addEdge(15, 11, 1);
+    mapBuilder::buildMap(g);
 
 
+    int treasureInput = 0;
+    int finalDest = 0;
     Graph::printMap();
+
+    std::cout << "Gazing across the battlefield, you activate the top secret "
+    "Omni-surface Navigable Treasure Hunting Robot from point 0 on the map..."
+    "Where is the treasure hidden: \n";
+    std::cin >> treasureInput;
+    while (treasureInput <= 0) {
+        std::cout << "Invalid Input -- X < 1";
+        std::cout << "\n Try again: ";
+        std::cin >> treasureInput;
+    }
+
+    std::cout << "Giving your trusty robot one last pat on the head and a 'Godspeed little guy'... "
+    "Where do you set the rendezvous point? \n";
+    std::cin >> finalDest;
+    while (finalDest < 0) {
+        std::cout << "Invalid Input -- X < 0";
+        std::cout << "\n Try again: ";
+        std::cin >> finalDest;
+    }
+
+
     dijkstraAlg dijkstra(g);
-    dijkstra.run(0, 3);
+    dijkstra.run(0, treasureInput);
+    dijkstra.run(treasureInput, finalDest);
 
 
 
